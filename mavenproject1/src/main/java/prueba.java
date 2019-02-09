4b4f92f98ffa5937afd286732b829c1dcadb28d0
BOG-A209-E-003 <BOG-A209-E-003@RGH-LS6-03.UMD.LOCAL.EDU>
2019-02-09 08:08:55 -0500

    CREACION JODA TIME

---
+++ mavenproject1/src/main/java/prueba.java
@@ -0,0 +1,22 @@
+
+import org.joda.time.LocalTime;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BOG-A209-E-003
+ */
+public class prueba {
+    
+ public static void main(String[] args){
+     LocalTime Tiempo= new LocalTime();
+     System.out.println("El tiempo es:"+Tiempo );
+ }
+
+}
